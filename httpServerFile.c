d5b23b5cce8aebb9d55fe89bd94b9ba0d41dfddd
Alex Dergach <C20401562@mytudublin.com>
2022-11-10 18:53:59 +0000

    Returned From Machine

--- httpServerFile.c
+++ httpServerFile.c
@@ -7,11 +7,14 @@
 #include <arpa/inet.h>
  #include "Practical.h"
 #include <unistd.h>
+
 #include <sys/stat.h> //NOTE THE INCLUSION OF A NEW HEADER FILE
+
 static const int MAXPENDING = 5; // Maximum outstanding connection requests
+
 int main (int argc, char *argv[]) {
 int numBytes = 0, char_in, count = 0, size = 0; // VARIABLES FOR FILE MANIPULATION
-char recvbuffer [BUFSIZE], sendbuffer [BUFSIZE], path[200] ={'.'), discard1 [50], discard2[50];
+char recvbuffer [BUFSIZE], sendbuffer [BUFSIZE], path[200] ={'.'}, discard1 [50], discard2[50];
 
 struct stat st;//STRUCTURE REQUIRED TO HOLD OPEN FILE ATTRIBUTES
 FILE * hFile; //FILE POINTER REQUIRED TO OPEN FILE
@@ -20,29 +23,29 @@ FILE * hFile; //FILE POINTER REQUIRED TO OPEN FILE
 	 if (strcmp (path, "./favicon.ico") == 0) //CHECK IF REQUEST IS FOR FAVICON - IGNORE AND CONTINUE TO NEXT ITERATION
      {
          printf ("\n\nFound favicon. .ico\n\n");
-         close (c1ntSock); // Close client socket
+         close (clntSock); // Close client socket
          continue;
 	 }
 
 	 if(strcmp (path, " ./index.html") == 0)//CHECK WHAT IS IN PATH
      {
-      snprintf(sendbuffer, sizeof(sendbuffer), HOME_PAGE);
-		}
-		else
-		{
-		snprintf(sendbuffer, sizeof(sendbuffer), ERROR_PAGE);
-		}
+    //   snprintf(sendbuffer, sizeof(sendbuffer), HOME_PAGE);
+	// 	}
+	// 	else
+	// 	{
+	// 	snprintf(sendbuffer, sizeof(sendbuffer), ERROR_PAGE);
+	// 	}
 
-		ssize_t numBytesSent = send(clntSock, sendbuffer, strlen(sendbuffer), 0);
+	// 	ssize_t numBytesSent = send(clntSock, sendbuffer, strlen(sendbuffer), 0);
 
-			if (numBytesSent < 0)
-				DieWithSystemMessage("send() failed");
+	// 		if (numBytesSent < 0) DieWithSystemMessage("send() failed");
 		
-		close(clntSock);
+	// 	close(clntSock);
       strcpy (path, "./index.html");
      }
 
      hFile = fopen (path, "r");//ATTEMPTING TO OPEN FILE IN PATH
+
      if (hFile == NULL)//IF REQUESTED FILE DOES NOT EXIST
               {
              //THIS IF SECTION ASSUMES REQUESTED FILE DOES NOT EXIST
@@ -73,6 +76,6 @@ FILE * hFile; //FILE POINTER REQUIRED TO OPEN FILE
 
 			  //RESET ALL VARIABLES AND BUFFERS(INCLUDING send/recvbuffers),CLOSE FILE AND CONNECTED SOCKET
 			  
-		}//END FOR LOOP
+		// }//END FOR LOOP
 
 }// END MAIN

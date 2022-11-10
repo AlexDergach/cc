870cdb4cdb64bed2861ac003825af7c37c0338d7
Alex Dergach <C20401562@mytudublin.com>
2022-11-10 18:28:48 +0000

    Returned From Machine

--- httpServerFile.c
+++ httpServerFile.c
@@ -5,101 +5,62 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "Practical.h"
+ #include "Practical.h"
 #include <unistd.h>
-#include <sys/stat.h>
-#define HOME_PAGE "HTTP/1.0 200 File Found\r\nContent-Length: 131\r\nConnection: close\r\nServer: httpserver\r\n\r\n<HTML><HEAD><TITLE>File  Found</TITLE></HEAD><BODY><h2>FILE Found</h2><hr><p>Your requested INDEX FILE was found.</p></BODY></HTML>"
-#define ERROR_PAGE "HTTP/1.0 404 File Not Found\r\nContent-Length: 142\r\nConnection: close\r\n\r\n<HTML><HEAD><TITLE>File NOT Found</TITLE></HEAD><BODY><h2>FILE NOT Found</h2><hr><p>Your requested INDEX FILE was NOT found.</p></BODY></HTML>"
+#include <sys/stat.h> //NOTE THE INCLUSION OF A NEW HEADER FILE
+static const int MAXPENDING = 5; // Maximum outstanding connection requests
+int main (int argc, char *argv[]) {
+int numBytes = 0, char_in, count = 0, size = 0; // VARIABLES FOR FILE MANIPULATION
+char recvbuffer [BUFSIZE], sendbuffer [BUFSIZE], path[200] ={'.'), discard1 [50], discard2[50];
 
+struct stat st;//STRUCTURE REQUIRED TO HOLD OPEN FILE ATTRIBUTES
+FILE * hFile; //FILE POINTER REQUIRED TO OPEN FILE
+     sscanf (recvbuffer, "%s %s %s", discard1, (path+1), discard2); //NOTE THE SECOND ELEMENT OF PATH IS REFERENCED
+     
+	 if (strcmp (path, "./favicon.ico") == 0) //CHECK IF REQUEST IS FOR FAVICON - IGNORE AND CONTINUE TO NEXT ITERATION
+     {
+         printf ("\n\nFound favicon. .ico\n\n");
+         close (c1ntSock); // Close client socket
+         continue;
+	 }
 
-static const int MAXPENDING = 3;
+	 if(strcmp (path, " ./") == 0)//CHECK WHAT IS IN PATH
+     {
+      IF ./ REPLACE WITH HOME PAGE FILE NAME
+      strcpy (path, "./index.html");
+     }
 
-int main(int argc, char *argv[]) {
+     hFile = fopen (path, "r");//ATTEMPTING TO OPEN FILE IN PATH
+     if (hFile == NULL)//IF REQUESTED FILE DOES NOT EXIST
+              {
+             //THIS IF SECTION ASSUMES REQUESTED FILE DOES NOT EXIST
+             //OPEN THE ERROR PAGE
+             strcpy (path, "./error.html");
+			 stat(filename,&st);
+			 size=st.st_size;//RETRIEVING FILE SIZE OF OPEN FILE 
+			 //STORENEGATIVEHTTPHEADERS(404RESPONSE)INOUTGOINGBUFFER
+			  }
+			  else
+			  {
+				//THISELSESECTIONASSUMESREQUESTEDFILEEXISTSANDISOPEN
+			    stat(path,&st);
+				size=st.st_size;//RETRIEVING FILE SIZE OF OPEN FILE
+			  	//STOREPOSITIVEHTTPHEADERS(200RESPONSE)INOUTGOINGBUFFER
+			  }
+			  //SENDHTTPHEADERSTOCONNECTEDSOCKET
+			  //RESETOUTGOINGBUFFER
+			  while((char_in=fgetc(hFile))!=EOF)//READING CONTENTS OF FILE CHARACTER-BY-CHARACTER
+			  {
+				sendbuffer[count]=char_in;//STORING EACH CHARACTER IN OUTGOING BUFFER
+				count++;
+			  }
 
-	int recvLoop = 0, numBytes = 0, totalBytes =0;
-	char recvbuffer[BUFSIZE], sendbuffer[BUFSIZE], uri[200] ={""}, discard1[50], discard2[50];
-	//Increased BUFSIZE to 1024 in the Practical.h file
-	char cmd[16];
-	char path[64];
-	char vers[16];
-	
- 	if (argc != 2) 
-    		DieWithUserMessage("Parameter(s)", "<Server Port>");
+			  sendbuffer[count]='\0';//NULL TERMINATE FILE CONTENTS
 
-  	in_port_t servPort = atoi(argv[1]); 
+			  //SENDFILECONTENTSTOCONNECTEDSOCKET
 
-	int servSock; 
+			  //RESETALLVARIABLESANDBUFFERS(INCLUDINGsend/recvbuffers),CLOSEFILEANDCONNECTEDSOCKET
+			  
+		}//END FOR LOOP
 
-  	if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-    		DieWithSystemMessage("socket() failed");
-
-  	struct sockaddr_in servAddr;
-  	memset(&servAddr, 0, sizeof(servAddr));       
-  	servAddr.sin_family = AF_INET;                
-  	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  	servAddr.sin_port = htons(servPort);
-  
-	if (bind(servSock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
-    		DieWithSystemMessage("bind() failed");
-
-  	if (listen(servSock, MAXPENDING) < 0)
-   	 	DieWithSystemMessage("listen() failed");
-
-	for (;;) { 
-
-    	int clntSock = accept(servSock, (struct sockaddr *) NULL, NULL);
-
-    	if (clntSock < 0)
-      		DieWithSystemMessage("accept() failed");
-
-		recvLoop = 1;
-		totalBytes = 0;
-		memset (uri, '\0', sizeof(uri));
-		memset (sendbuffer, '\0', sizeof(sendbuffer));
-		memset (recvbuffer, '\0', sizeof(recvbuffer));
-		
-		while (recvLoop  > 0) 
-		{  
-
-			numBytes = recv(clntSock, (recvbuffer+totalBytes), 1, 0); //note the one byte limitation -  argument three	
-			totalBytes += numBytes; //updating the off-set
-
-			if((totalBytes >= (BUFSIZE-2)) || (strstr(recvbuffer,"\r\n\r\n")>0))
-				recvLoop = 0;  //do not exceed the size of the recvbuffer OR looking for \r\n\r\n
-					
-		}
-		if (numBytes < 0)
-				DieWithSystemMessage("recv() failed");
-
-		sscanf(recvbuffer, "%s %s %s\r\n", discard1, uri, discard2);  //parsing the incoming stream
-
-		if(strcmp(uri, "/favicon.ico") == 0)
-		{
-			printf("\n\nFound and ignored favicon.ico\n\n");
-			close(clntSock);
-
-		}
-		else{
-			
-		recvbuffer[totalBytes] = '\0'; 
-		fputs(recvbuffer, stdout);
-
-		if(strcmp(uri, "/index.html") == 0)
-		{
-		snprintf(sendbuffer, sizeof(sendbuffer), HOME_PAGE);
-		}
-		else
-		{
-		snprintf(sendbuffer, sizeof(sendbuffer), ERROR_PAGE);
-		}
-
-		ssize_t numBytesSent = send(clntSock, sendbuffer, strlen(sendbuffer), 0);
-
-			if (numBytesSent < 0)
-				DieWithSystemMessage("send() failed");
-		
-		close(clntSock);
-		}
-	  }
-  
-}
+}// END MAIN
